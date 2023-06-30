9848029e236315b758b925fb1a39b47c39045b47
GarshNata <128638500+GarshNata@users.noreply.github.com>
2023-06-30 21:28:55 +0300

    homework 20

--- homework-20/js/dist/h.w.20.dev.js
+++ homework-20/js/dist/h.w.20.dev.js
@@ -6,5 +6,6 @@ var sum = +firstSalary + +secondSalary;
 alert("Your salery for 2 month is: ".concat(sum, "$"));
 var bonus = +sum + 1;
 alert("You have extra bonus! Your total salary was: ".concat(sum, "$ it's increase for 1$ and now it is: ").concat(bonus, "$"));
-alert("Will you work next? " + (sum >= 2000) || "Will you work next? " + (sum < 2000));
-sum >= 2000 ? alert("I'm ready to work") : alert("I'm out");
+alert("Will you work next? " + (bonus >= 2000) || "Will you work next? " + (bonus < 2000));
+bonus >= 2000 && alert("I'm ready to work!");
+bonus < 2000 && alert("I'm out");
--- homework-20/js/h.w.20.js
+++ homework-20/js/h.w.20.js
@@ -4,5 +4,6 @@ let sum = +firstSalary + +secondSalary;
 alert (`Your salery for 2 month is: ${sum}$`);
 let bonus = + sum + 1;
 alert (`You have extra bonus! Your total salary was: ${sum}$ it's increase for 1$ and now it is: ${bonus}$`);
-alert ("Will you work next? " + (sum >=2000) || "Will you work next? " + (sum <2000));
-sum >= 2000 ? alert("I'm ready to work") : alert("I'm out");
+alert ("Will you work next? " + (bonus >=2000) || "Will you work next? " + (bonus <2000));
+bonus >= 2000 && alert("I'm ready to work!");
+bonus < 2000 && alert("I'm out");
