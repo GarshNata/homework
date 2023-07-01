da39df24c867908d5f5dc0eeb6106636b1f7bce0
GarshNata <128638500+GarshNata@users.noreply.github.com>
2023-07-01 13:47:36 +0300

    homework 21

---
+++ homework-21/js/dist/h.w.20.dev.js
@@ -0,0 +1,3 @@
+"use strict";
+
+confirm("Do you want we call you?") ? alert("We are calling") : alert("OK. We don't disturb you");
---
+++ homework-21/js/h.w.21.js
@@ -0,0 +1,56 @@
+function confirm(age) {
+    if (age <= 17) {
+      return prompt("Do you study at school?");
+    }
+    if (age > 17 && age < 25) {
+      return prompt("Do you study at university?");
+    }
+    if (age >= 25 && age < 60) {
+      return prompt("Do you have a job?");
+    }
+    return prompt("What are you doing?");
+  }
+  let age = parseInt(prompt("Enter your age:"));
+  let response = confirm(age);
+  console.log("Answer:", response);
+
+
+  function taskTwo() {
+    let input = prompt("Enter value CSS property Display (block, flex, grid, inline, none):");
+    switch (input) {
+      case "block":
+        alert("Property selected display: block;");
+        break;
+      case "flex":
+        alert("Property selected display: flex;");
+        break;
+      case "grid":
+        alert("Property selected display: grid;");
+        break;
+      case "inline":
+        alert("Property selected display: inline;");
+        break;
+      case "none":
+        alert("Property selected display: none;");
+        break;
+      default:
+        alert("Set 'display: inline-block;'");
+    }
+  }
+  taskTwo();
+
+
+  function taskThree() {
+    let input = prompt("Please enter the number");
+    while (isNaN(input) || Number(input) <= 0) {
+      input = prompt("Your number is incorect, please enter corect number");
+    }
+    let result = Number(input) * 12;
+    alert(+ input + " by 12 equals " + result);
+  }
+  taskThree();
+  
+
+  for (var i = 2; i <= 50; i += 2) {
+    console.log(i);
+  }
