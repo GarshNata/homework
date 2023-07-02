d97d9b8aa84c6b951029b8a1763b612935cb00b6
GarshNata <128638500+GarshNata@users.noreply.github.com>
2023-07-02 14:49:48 +0300

    homework 22

---
+++ homework-22/js/dist/h.w.22.dev.js
@@ -0,0 +1,63 @@
+"use strict";
+
+function maxNumber(a, b) {
+  if (a > b) {
+    return a;
+  } else if (b > a) {
+    return b;
+  } else {
+    return "a=b";
+  }
+}
+
+var a = prompt("Enter the number a:");
+var b = prompt("Enter the number b:");
+alert(maxNumber(a, b));
+
+function reverse(number) {
+  return -number;
+}
+
+var numb = prompt("Enter the number:");
+alert(reverse(numb));
+
+function three(number, count) {
+  for (var i = 0; i < count; i++) {
+    number += 3;
+  }
+
+  return number;
+}
+
+var number = prompt("Введите число:");
+var count = prompt("Введите количество добавлений:");
+alert(three(+number, +count));
+
+function KmToM(km) {
+  return km * 1000;
+}
+
+function KmToCm(km) {
+  return km * 100000;
+}
+
+function getMetric(metric, km, m, cm) {
+  var result;
+
+  if (metric === "m") {
+    result = m(KmToM(km));
+  } else if (metric === "cm") {
+    result = cm(KmToCm(km));
+  }
+
+  return result;
+}
+
+var km = prompt("Enter value in kilometers:");
+var metric = prompt("Enter metric ('m' или 'cm'):");
+var result = getMetric(metric, km, function (meters) {
+  return meters;
+}, function (centimeters) {
+  return centimeters;
+});
+alert(result);
---
+++ homework-22/js/h.w.22.js
@@ -0,0 +1,55 @@
+function maxNumber(a, b) {
+  if (a > b) {
+    return a;
+  } else if (b > a) {
+    return b;
+  } else {
+    return "a=b";
+  }
+}
+let a = prompt("Enter the number a:");
+let b = prompt("Enter the number b:");
+alert(maxNumber(a, b));
+
+
+function reverse(number) {
+  return -number;
+}
+let numb = prompt("Enter the number:");
+alert(reverse(numb));
+
+
+function three(number, count) {
+  for (let i = 0; i < count; i++) {
+    number += 3;
+  }
+  return number;
+}
+let number = prompt("Введите число:");
+let count = prompt("Введите количество добавлений:");
+alert(three(+number, +count));
+
+
+function KmToM(km) {
+  return km * 1000;
+}
+function KmToCm(km) {
+  return km * 100000;
+}
+function getMetric(metric, km, m, cm) {
+  let result;
+  if (metric === "m") {
+    result = m(KmToM(km));
+  } else if (metric === "cm") {
+    result = cm(KmToCm(km));
+  }
+  return result;
+}
+let km = prompt("Enter value in kilometers:");
+let metric = prompt("Enter metric ('m' или 'cm'):");
+let result = getMetric(metric, km, function(meters) {
+  return meters;
+}, function(centimeters) {
+  return centimeters;
+});
+alert(result);
